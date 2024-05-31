b282b9bc8ce09ea2f544f633d6894dfe273dde0c
Amantsehay <amanueltsehay11@gmail.com>
2024-06-01 00:23:10 +0300

    deployment stage

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["https://mernchatwithsocketio-2.onrender.com"],
 		methods: ["GET", "POST"],
 		cors: 'Allow-Control-Allow-Origin',
 	},
